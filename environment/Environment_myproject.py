219f4f4b320354e5bc451606d60e14830e532833
Yotghoe <167158439+Yotghoe@users.noreply.github.com>
2024-05-14 21:49:37 +1000

    Update Environment_myproject.py

--- environment/Environment_myproject.py
+++ environment/Environment_myproject.py
@@ -1,13 +1,14 @@
 #Create an environment with python 3.10
 conda create -n myproject python=3.10
 conda activate myproject
-#Install geopandas and pandas
-pip install pandas geopandas
+#Install packages
+pip install pandas geopandas shapely
 #Test of python version
 python3
 #Test of whether pandas and geopandas are installed: If there are no errors, and the interpreter returns to the next line without any messages, it means that both pandas and geopandas have been successfully installed and are accessible in the environment.You don't have to type ">>>" if they already exist.
 >>> import pandas
 >>> import geopandas
+>>> import shapely
 
 #Install OpenQuake Engine
 #Step 1
